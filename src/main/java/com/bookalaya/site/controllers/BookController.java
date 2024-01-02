f7c0cfa4efea9097626ad133feddfc81ea14ba9d
Your Name <rachit.gupta97@yahoo.com>
2024-01-02 19:55:20 +0530

    Initial Commit Added basic homepage.

---
+++ src/main/java/com/bookalaya/site/controllers/BookController.java
@@ -0,0 +1,17 @@
+package com.bookalaya.site.controllers;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+public class BookController {
+
+    @RequestMapping("/test")
+    public String display()
+    {
+        return "index";
+    }
+}
